07dd7826f9932f8f69d5c98790f5e708d70a686f
Antonio Ávila <81250968+koavdev@users.noreply.github.com>
2023-09-14 11:29:55 -0300

    feat: Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="pokedex">
+    <h1 class="text--yellow">Pokedex DIO</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style lang="scss">
+  #pokedex {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    max-width: 100px;
+    margin: 0 auto;
+    padding: 24px;
+
+  }
+</style>
